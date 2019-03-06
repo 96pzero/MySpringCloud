632b1f6649d7ab20b2dcf9935f410e9e1ff99d74
96pzero <46878251+96pzero@users.noreply.github.com>
2019-03-06 11:35:39 +0800

    Update MemberController.java
    增加了一个读取配置文件的变量。

--- SpringCloud-Eureka/spring-cloud-member/src/main/java/com/zero/controller/MemberController.java
+++ SpringCloud-Eureka/spring-cloud-member/src/main/java/com/zero/controller/MemberController.java
@@ -3,18 +3,22 @@ package com.zero.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-
+	
+	@Value("${server.port}")
+	private String port;
 	@RequestMapping("/getMemberAll")
 	public List<String> getMemberAll(){
 		List<String> userlist = new ArrayList<String>();
 		userlist.add("张三1");
 		userlist.add("张三2");
 		userlist.add("张三3");
+		userlist.add("serverPort:"+port);
 		return userlist;
 	}
 }
