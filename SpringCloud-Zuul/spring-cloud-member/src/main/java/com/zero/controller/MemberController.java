f44499ec89eb80bf4dd10f9aef5a6c4a56d1a9e4
96pzero <46878251+96pzero@users.noreply.github.com>
2019-03-07 11:46:43 +0800

    Revert "Revert "Change MySpringCloud Resposity""
    This reverts commit c29cc58894be47389b073ae484bf0773dda8c23d.

---
+++ SpringCloud-Zuul/spring-cloud-member/src/main/java/com/zero/controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.zero.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+	
+	@Value("${server.port}")
+	private String port;
+	@RequestMapping("/getMemberAll")
+	public List<String> getMemberAll(){
+		List<String> userlist = new ArrayList<String>();
+		userlist.add("张三1");
+		userlist.add("张三2");
+		userlist.add("张三3");
+		userlist.add("serverPort:"+port);
+		return userlist;
+	}
+	
+	@RequestMapping("/getMemberServiceApi")
+	public String getMemberServiceApi() {
+		return "this is 会员 服务工程";
+	}
+}
